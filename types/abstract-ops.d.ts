49f048f056e13d27893ab6966a4a9f1984c4094a
KallynGowdy <kallyngowdy@gmail.com>
2022-10-04 13:10:29 -0400

    Add definitions for common object and function operations

--- types/abstract-ops.d.ts
+++ types/abstract-ops.d.ts
@@ -6,9 +6,25 @@ import { BooleanValue, ObjectValue, Value, JSStringValue, SymbolValue, NumberVal
 // Object Operations
 export function MakeBasicObject(internalSlotsList: string[]): ObjectValue;
 export function Get(O: Value, P: Value): Generator<unknown, Completion<Value>, unknown>;
+export function GetV(V: Value, P: Value): Generator<unknown, Value | Completion<any>, unknown>;
 export function Set(O: Value, P: Value, V: Value, Throw: BooleanValue): Generator<unknown, Completion<Value>, unknown>;
 export function CreateDataProperty(O: Value, P: Value, V: Value): Completion<Value>;
 
+export function DefinePropertyOrThrow(O: Value, P: Value, desc: any): BooleanValue | Completion<any>;
+export function DeletePropertyOrThrow(O: Value, P: Value): BooleanValue | Completion<any>;
+export function GetMethod(V: Value, P: Value): ObjectValue | Completion<any>;
+export function HasProperty(O: Value, P: Value): BooleanValue | Completion<any>;
+export function HasOwnProperty(O: Value, P: Value): BooleanValue | Completion<any>;
+
+export function CreateArrayFromList(elements: Value[]): ObjectValue;
+export function LengthOfArrayLike(obj: ObjectValue): number;
+export function CreateListFromArrayLike(obj: ObjectValue): ObjectValue;
+
+export function Call(F: ObjectValue, V: Value, argumentsList: Value[]): Generator<unknown,Completion<Value>, unknown>;
+export function Construct(F: ObjectValue, argumentsList: Value[], newTarget?: ObjectValue): Generator<unknown, Completion<Value>, unknown>;
+export function Invoke(V: ObjectValue, P: Value, argumentsList: Value[]): Generator<unknown, Completion<Value>, unknown>;
 
 // Function Operations
 export function CreateBuiltinFunction(steps: Function, length: number, name: JSStringValue | SymbolValue, realm: Realm, prototype?: ObjectValue, prefix?: JSStringValue, isConstructor?: BooleanValue): ObjectValue;
+export function isECMAScriptFunctionObject(O: unknown): boolean;
+export function isFunctionObject(O: unknown): boolean;
