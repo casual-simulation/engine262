09a0d09bf6d1bddea905aecc600e042bfef7f6e5
KallynGowdy <kallyngowdy@gmail.com>
2022-10-10 15:36:46 -0400

    Add the ability to yield before a node is evaluated and after it is evaluated

--- types/evaluator.d.ts
+++ types/evaluator.d.ts
@@ -2,4 +2,17 @@ import { Completion } from "./completion";
 import { ECMAScriptNode }  from './parse';
 import { Value } from './value';
 
-export function Evaluate(node: ECMAScriptNode): Generator<any, Completion<Value>, any>;
+export type EvaluationYield = BeforeEvaluationYield | AfterEvaluationYield;
+
+export interface BeforeEvaluationYield {
+    evaluationState: 'before';
+    node: ECMAScriptNode;
+}
+
+export interface AfterEvaluationYield {
+    evaluationState: 'after';
+    node: ECMAScriptNode;
+    result: Value;
+}
+
+export function Evaluate(node: ECMAScriptNode): Generator<EvaluationYield, Completion<Value>, any>;
